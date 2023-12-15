e8cd8d19b957606622305173f0b9c5ec3a3684bf
ankitha-anil <ankitha.anil@gmail.com>
2023-12-15 13:42:41 +0800

    Added delete and undo portion. Need to settle showing snackbar

--- app/src/main/java/com/example/taskmanager/tasks/TaskViewModel.kt
+++ app/src/main/java/com/example/taskmanager/tasks/TaskViewModel.kt
@@ -8,9 +8,11 @@ import com.example.taskmanager.data.SortOrder
 import com.example.taskmanager.data.Task
 import com.example.taskmanager.data.TaskDao
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -23,31 +25,47 @@ class TaskViewModel @Inject constructor(
     val searchQuery = MutableStateFlow("")
     val preferenceFlow = preferencesManager.preferencesFlow
 
+    private val tasksEventChannel = Channel<TaskEvent>()
+    val taskEvent = tasksEventChannel.receiveAsFlow() // receive as flow
+
     private val tasksFlow = combine(
         searchQuery,
         preferenceFlow
-    ){
-        query, filterPreferences->
-            Pair(query, filterPreferences)
-    }.flatMapLatest{ (query, filterPreferences) ->
+    ) { query, filterPreferences ->
+        Pair(query, filterPreferences)
+    }.flatMapLatest { (query, filterPreferences) ->
         taskDao.getTasks(query, filterPreferences.sortOrder, filterPreferences.hideCompleted)
     }
-    val tasks = tasksFlow.asLiveData() //live data is similar to flow, live data has latest value and not whole stream of values
+    val tasks =
+        tasksFlow.asLiveData() //live data is similar to flow, live data has latest value and not whole stream of values
 
     fun onSortOrderSelected(sortOrder: SortOrder) = viewModelScope.launch {
         preferencesManager.updateSortOrder(sortOrder)
     }
+
     fun onHideCompletedClick(hideCompleted: Boolean) = viewModelScope.launch {
         preferencesManager.updateHideCompleted(hideCompleted)
     }
-    fun onTaskSelected(task: Task){}
 
-    fun onTaskChecked(task: Task, isChecked: Boolean){
+    fun onTaskSelected(task: Task) {}
+
+    fun onTaskChecked(task: Task, isChecked: Boolean) {
         viewModelScope.launch {
             taskDao.update(task.copy(checked = isChecked))
         }
-
     }
 
+    fun onTaskSwiped(task: Task) = viewModelScope.launch {
+        taskDao.delete(task)
+        tasksEventChannel.send(TaskEvent.ShowUndoDeleteTaskMessage(task))
+    }
+
+    fun onUndoDeleteClick(task: Task) = viewModelScope.launch {
+        taskDao.insert(task)
+    }
+
+    sealed class TaskEvent {  //different variation, can later get warning when the when statement is not exhaustive, there are no other kinds of task events compiler know
+        data class ShowUndoDeleteTaskMessage(val task: Task) : TaskEvent() // generic name cause viewmodel not sure of the view
+    }
 }
 
--- app/src/main/java/com/example/taskmanager/view/HomeFragment.kt
+++ app/src/main/java/com/example/taskmanager/view/HomeFragment.kt
@@ -11,6 +11,7 @@ import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.taskmanager.R
@@ -21,6 +22,7 @@ import com.example.taskmanager.tasks.TaskViewModel
 import com.example.taskmanager.databinding.FragmentHomeBinding
 import com.example.taskmanager.util.onQueryTextChanged
 import dagger.hilt.android.AndroidEntryPoint
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 
@@ -54,6 +56,22 @@ class HomeFragment : Fragment(R.layout.fragment_home), TaskAdapter.onItemClickLi
                 setHasFixedSize(true)
 
             }
+            ItemTouchHelper(object :
+                ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
+                override fun onMove(
+                    recyclerView: RecyclerView,
+                    viewHolder: RecyclerView.ViewHolder,
+                    target: RecyclerView.ViewHolder
+                ): Boolean {
+                    return false
+                }
+
+                override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                    val task = taskAdapter.currentList[viewHolder.adapterPosition]
+                    viewModel.onTaskSwiped(task)
+                }
+
+            }).attachToRecyclerView(recyclerViewTasks)
         }
         taskAdapter.registerAdapterDataObserver(object : RecyclerView.AdapterDataObserver() {
             override fun onItemRangeMoved(fromPosition: Int, toPosition: Int, itemCount: Int) {
@@ -64,6 +82,20 @@ class HomeFragment : Fragment(R.layout.fragment_home), TaskAdapter.onItemClickLi
         viewModel.tasks.observe(viewLifecycleOwner) {
             taskAdapter.submitList(it)
         }
+
+        viewLifecycleOwner.lifecycleScope.launchWhenStarted {
+            viewModel.taskEvent.collect { event ->
+                when(event){
+                    is TaskViewModel.TaskEvent.ShowUndoDeleteTaskMessage -> {
+                        Snackbar.make(requireView(), "Task deleted", Snackbar.LENGTH_LONG)
+                            .setAction("UNDO"){
+                                viewModel.onUndoDeleteClick(event.task)
+                            }. show()
+                    }
+                }
+            }
+        }
+
         setHasOptionsMenu(true)
 
     }
@@ -78,29 +110,34 @@ class HomeFragment : Fragment(R.layout.fragment_home), TaskAdapter.onItemClickLi
         }
 
         viewLifecycleOwner.lifecycleScope.launch {
-            menu.findItem(R.id.hide_completed_tasks).isChecked = viewModel.preferenceFlow.first().hideCompleted
+            menu.findItem(R.id.hide_completed_tasks).isChecked =
+                viewModel.preferenceFlow.first().hideCompleted
         }
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-       return when(item.itemId){
+        return when (item.itemId) {
             R.id.sort_by_name -> {
                 viewModel.onSortOrderSelected(SortOrder.BY_NAME)
-                 true
+                true
             }
+
             R.id.sort_by_date_created -> {
                 viewModel.onSortOrderSelected(SortOrder.BY_DATE)
-                 true
+                true
             }
-           R.id.hide_completed_tasks -> {
-               item.isChecked = !item.isChecked
-               viewModel.onHideCompletedClick(item.isChecked)
-               true
-           }
-           R.id.delete_all_completed_tasks -> {
-                   true
-           }
-           else -> super.onOptionsItemSelected(item)
+
+            R.id.hide_completed_tasks -> {
+                item.isChecked = !item.isChecked
+                viewModel.onHideCompletedClick(item.isChecked)
+                true
+            }
+
+            R.id.delete_all_completed_tasks -> {
+                true
+            }
+
+            else -> super.onOptionsItemSelected(item)
         }
     }
 
