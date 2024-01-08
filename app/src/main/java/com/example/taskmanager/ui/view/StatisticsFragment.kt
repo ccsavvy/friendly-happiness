c6e7bdef8ec9d6756e56a39bdd8a92ab0e7ab3a6
ankitha-anil <ankitha.anil@gmail.com>
2024-01-08 12:09:24 +0800

    Cleaned up code for StatisticsFragment and deleted AuthorisationManager

--- app/src/main/java/com/example/taskmanager/ui/view/StatisticsFragment.kt
+++ app/src/main/java/com/example/taskmanager/ui/view/StatisticsFragment.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.taskmanager.MainActivity
@@ -29,54 +31,25 @@ class StatisticsFragment : Fragment(R.layout.fragment_statistics) {
     private val viewModel: StatisticsViewModel by viewModels()
     lateinit var pieChart: PieChart
 
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        getUser()
-        registerObservers()
         binding = FragmentStatisticsBinding.inflate(inflater)
         pieChart = binding.pieChart
-        // val progress = binding.progress
-        pieChart()
-        setData()
+        getUser()
+        registerObservers()
         return binding.root
     }
 
-    fun pieChart() {
+    fun initPieChart() {
         StatsChart().setUI(pieChart)
     }
 
-    fun setData() {
-        // add a lot of colors to list
-        val colors: ArrayList<Int> = ArrayList()
-        colors.add(resources.getColor(R.color.purple_200))
-        colors.add(resources.getColor(R.color.grey_100))
-        colors.add(resources.getColor(R.color.green_200))
-
-        val entries: ArrayList<PieEntry> = ArrayList()
-
-        viewModel.completedTasks.observe(viewLifecycleOwner) {
-            val completedTask = viewModel.completedTaskNum.toFloat()
-            val totalTask = viewModel.taskNum.toFloat()
-            var num = (completedTask / totalTask * 100).toInt()
-            //progress.progress = num
-
-            entries.add(PieEntry(completedTask))
-            entries.add(PieEntry(totalTask - completedTask))
-            val dataSet = PieDataSet(entries, "Tasks")
-
-            pieChart.centerText = "$num% \nCompleted"
-
-            //  setting colors.
-            dataSet.colors = colors
-
-            //  setting pie data set
-            val data = PieData(dataSet)
-            pieChart.setData(data)
-            pieChart.data.setDrawValues(false)
-        }
+    fun setChartData() {
+        pieChart.centerText = viewModel.centerText
+        pieChart.data = viewModel.data.value
+        pieChart.data.setDrawValues(false)
     }
 
     fun getUser() {
@@ -90,7 +63,6 @@ class StatisticsFragment : Fragment(R.layout.fragment_statistics) {
                 viewModel.computeStatistics()
             } ?: startActivity(Intent(requireContext(), MainActivity::class.java))
         }
-
         viewModel.totalTasks.observe(viewLifecycleOwner) {
             binding.apply {
                 totalTasks.text = viewModel.taskNum.toString()
@@ -101,6 +73,11 @@ class StatisticsFragment : Fragment(R.layout.fragment_statistics) {
                 completedTasks.text = viewModel.completedTaskNum.toString()
             }
         }
+        viewModel.data.observe(viewLifecycleOwner) {
+            binding.apply {
+                initPieChart()
+                setChartData()
+            }
+        }
     }
-
 }
--- app/src/main/java/com/example/taskmanager/viewModel/StatisticsViewModel.kt
+++ app/src/main/java/com/example/taskmanager/viewModel/StatisticsViewModel.kt
@@ -1,13 +1,24 @@
 package com.example.taskmanager.viewModel
 
+import android.app.Application
+import android.graphics.Color
+import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.core.content.ContextCompat
+import androidx.core.content.res.ResourcesCompat
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.taskmanager.R
 import com.example.taskmanager.auth.AuthRepository
 import com.example.taskmanager.data.Task
 import com.example.taskmanager.data.TaskDao
+import com.example.taskmanager.di.ApplicationScope
+import com.github.mikephil.charting.data.PieData
+import com.github.mikephil.charting.data.PieDataSet
+import com.github.mikephil.charting.data.PieEntry
 import com.google.firebase.auth.FirebaseUser
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -15,16 +26,19 @@ import javax.inject.Inject
 @HiltViewModel
 class StatisticsViewModel @Inject constructor(
     private val taskDao: TaskDao,
-    private val repository: AuthRepository
-) : ViewModel() {
+    private val repository: AuthRepository,
+    ) : ViewModel() {
 
     private val firebaseUser = MutableLiveData<FirebaseUser?>()
     val currentUser get() = firebaseUser
+
+    var centerText: String = ""
 
     var totalTasks = MutableLiveData<List<Task>>()
     var completedTasks = MutableLiveData<List<Task>>()
     var taskNum = 0
     var completedTaskNum = 0
+    var data = MutableLiveData<PieData>()
 
     fun computeStatistics() {
         viewModelScope.launch {
@@ -39,13 +53,34 @@ class StatisticsViewModel @Inject constructor(
                     completedTasks.postValue(list)
                     true
                 }
+                setChartData()
             }
         }
+    }
+
+    fun setChartData() {
+        val entries: ArrayList<PieEntry> = ArrayList()
+        val completedTask = completedTaskNum.toFloat()
+        val totalTask = taskNum.toFloat()
+        val num = (completedTask / totalTask * 100).toInt()
+
+        entries.add(PieEntry(completedTask))
+        entries.add(PieEntry(totalTask - completedTask))
+        var dataSet = PieDataSet(entries, "Tasks")
+
+        centerText = "$num% \nCompleted"
+
+        val colors: ArrayList<Int> = ArrayList()
+        colors.add(Color.rgb(187, 134, 252))
+        colors.add(Color.rgb(116, 116, 116))
+        colors.add(R.color.green_200)
+
+        dataSet.colors = colors
+        data.postValue(PieData(dataSet))
+
     }
 
     fun getCurrentUser() = viewModelScope.launch {
         firebaseUser.postValue(repository.getCurrentUser())
     }
-
-
 }
